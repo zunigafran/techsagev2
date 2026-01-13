f1e78341db8dd9b51c89a69e9081101e391c66b2
Francisco Zuniga <118315442+zunigafran@users.noreply.github.com>
2026-01-12 20:42:46 -0500

    mainpage change

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -6,7 +6,7 @@ interface HeroProps {
 
 const Hero = ({ id }: HeroProps) => {
   return (
-    <div id={id} className="relative py-32 content-start overflow-x-hidden overflow-y-visible translate-y-1/4 w-screen -mx-2 lg:-mx-24">
+    <div id={id} className="relative py-32 content-start overflow-x-hidden overflow-y-visible translate-y-1/4 w-screen -mx-4 lg:-mx-24">
       <Gradient/>
       <div className="relative z-10">
         <h1 className="text-4xl font-bold text-center md:text-8xl md:text-left px-4 lg:px-24">
